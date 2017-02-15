783c3d7af4c19330f34c83b82da42fa011374e10
Sébastien Lesaint <sebastien.lesaint@sonarsource.com>
2017-02-15 14:32:21 +0100

    SONAR-8728 fix MassUpdate#Multihandler implementation
    to open at most PreparedStatement at a time

--- sonar-db/src/main/java/org/sonar/db/version/MassUpdate.java
+++ sonar-db/src/main/java/org/sonar/db/version/MassUpdate.java
@@ -19,12 +19,17 @@
  */
 package org.sonar.db.version;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
+import javax.annotation.Nullable;
 import org.sonar.core.util.ProgressLogger;
+import org.sonar.db.BatchSession;
 import org.sonar.db.Database;
 
 import static com.google.common.base.Preconditions.checkState;
@@ -59,7 +64,7 @@ public class MassUpdate {
   private final ProgressLogger progress = ProgressLogger.create(getClass(), counter);
 
   private Select select;
-  private List<UpsertImpl> updates = new ArrayList<>(1);
+  private List<String> updateStatements = new ArrayList<>(1);
 
   MassUpdate(Database db, Connection readConnection, Connection writeConnection) {
     this.db = db;
@@ -73,7 +78,7 @@ public class MassUpdate {
   }
 
   public MassUpdate update(String sql) throws SQLException {
-    this.updates.add(UpsertImpl.create(writeConnection, sql));
+    this.updateStatements.add(sql);
     return this;
   }
 
@@ -83,28 +88,14 @@ public class MassUpdate {
   }
 
   public void execute(Handler handler) throws SQLException {
-    checkState(select != null && !updates.isEmpty(), "SELECT or UPDATE requests are not defined");
-    checkState(updates.size() == 1, "There should be only one update when using a " + Handler.class.getName());
+    checkState(select != null && !updateStatements.isEmpty(), "SELECT or UPDATE requests are not defined");
+    checkState(updateStatements.size() == 1, "There should be only one update when using a " + Handler.class.getName());
 
     progress.start();
     try {
-      select.scroll(row -> callSingleHandler(handler, updates.iterator().next(), row));
-      closeUpdates();
-
-      // log the total number of processed rows
-      progress.log();
-    } finally {
-      progress.stop();
-    }
-  }
-
-  public void execute(MultiHandler handler) throws SQLException {
-    checkState(select != null && !updates.isEmpty(), "SELECT or UPDATE(s) requests are not defined");
-
-    progress.start();
-    try {
-      select.scroll(row -> callMultiHandler(handler, updates, row));
-      closeUpdates();
+      UpsertImpl update = UpsertImpl.create(writeConnection, updateStatements.iterator().next());
+      select.scroll(row -> callSingleHandler(handler, update, row));
+      closeUpsertImpl(update);
 
       // log the total number of processed rows
       progress.log();
@@ -120,24 +111,143 @@ public class MassUpdate {
     counter.getAndIncrement();
   }
 
-  private void callMultiHandler(MultiHandler handler, List<UpsertImpl> updates, Select.Row row) throws SQLException {
-    int i = 0;
-    for (UpsertImpl update : updates) {
-      if (handler.handle(row, update, i)) {
-        update.addBatch();
+  public void execute(MultiHandler handler) throws SQLException {
+    checkState(select != null && !updateStatements.isEmpty(), "SELECT or UPDATE(s) requests are not defined");
+
+    progress.start();
+    try {
+      try (MultiHandlerRowHandler rowHandler = new MultiHandlerRowHandler(handler)) {
+        select.scroll(rowHandler);
       }
-      i++;
+
+      // log the total number of processed rows
+      progress.log();
+    } finally {
+      progress.stop();
     }
-    counter.getAndIncrement();
   }
 
-  private void closeUpdates() throws SQLException {
-    for (UpsertImpl update : updates) {
-      if (update.getBatchCount() > 0L) {
-        update.execute().commit();
-      }
-      update.close();
+  private class MultiHandlerRowHandler implements Select.RowHandler, AutoCloseable {
+    private final Multimap<String, SqlStatementBufferingProxy> proxiesPerUpdateStatement = ArrayListMultimap.create();
+    private final MultiHandler handler;
+    private int rowCount = 0;
+
+    private MultiHandlerRowHandler(MultiHandler handler) {
+      this.handler = handler;
     }
+
+    @Override
+    public void handle(Select.Row row) throws SQLException {
+      if (rowCount == BatchSession.MAX_BATCH_SIZE) {
+        executeStatements();
+      }
+      int statementIndex = 0;
+      for (String updateStatement : updateStatements) {
+        SqlStatementBufferingProxy buffer = new SqlStatementBufferingProxy();
+        if (handler.handle(row, buffer, statementIndex)) {
+          proxiesPerUpdateStatement.put(updateStatement, buffer);
+        }
+        statementIndex++;
+      }
+      rowCount++;
+    }
+
+    @Override
+    public void close() throws SQLException {
+      if (rowCount > 0) {
+        executeStatements();
+      }
+    }
+
+    private void executeStatements() throws SQLException {
+      for (String updateStatement : updateStatements) {
+        UpsertImpl update = UpsertImpl.create(writeConnection, updateStatement);
+        for (SqlStatementBufferingProxy bufferingProxy : proxiesPerUpdateStatement.get(updateStatement)) {
+          bufferingProxy.accept(update);
+          update.addBatch();
+        }
+        closeUpsertImpl(update);
+      }
+
+      counter.addAndGet(rowCount);
+      rowCount = 0;
+    }
+  }
+
+  @FunctionalInterface
+  private interface SqlConsumer<T> {
+    void accept(T t) throws SQLException;
+  }
+
+  private static final class SqlStatementBufferingProxy implements SqlStatement<SqlStatementBufferingProxy>, SqlConsumer<SqlStatement> {
+    private final List<SqlStatementSetterCall> setterCalls = new ArrayList<>();
+
+    @Override
+    public void accept(SqlStatement preparedStatement) throws SQLException {
+      for (SqlStatementSetterCall setterCall : setterCalls) {
+        setterCall.execute(preparedStatement);
+      }
+    }
+
+    @Override
+    public SqlStatementBufferingProxy setBoolean(int columnIndex, @Nullable Boolean value) throws SQLException {
+      setterCalls.add(pstmt -> pstmt.setBoolean(columnIndex, value));
+      return this;
+    }
+
+    @Override
+    public SqlStatementBufferingProxy setDate(int columnIndex, @Nullable Date value) throws SQLException {
+      setterCalls.add(pstmt -> pstmt.setDate(columnIndex, value));
+      return this;
+    }
+
+    @Override
+    public SqlStatementBufferingProxy setDouble(int columnIndex, @Nullable Double value) throws SQLException {
+      setterCalls.add(pstmt -> pstmt.setDouble(columnIndex, value));
+      return this;
+    }
+
+    @Override
+    public SqlStatementBufferingProxy setInt(int columnIndex, @Nullable Integer value) throws SQLException {
+      setterCalls.add(pstmt -> pstmt.setInt(columnIndex, value));
+      return this;
+    }
+
+    @Override
+    public SqlStatementBufferingProxy setLong(int columnIndex, @Nullable Long value) throws SQLException {
+      setterCalls.add(pstmt -> pstmt.setLong(columnIndex, value));
+      return this;
+    }
+
+    @Override
+    public SqlStatementBufferingProxy setString(int columnIndex, @Nullable String value) throws SQLException {
+      setterCalls.add(pstmt -> pstmt.setString(columnIndex, value));
+      return this;
+    }
+
+    @Override
+    public SqlStatementBufferingProxy setBytes(int columnIndex, @Nullable byte[] data) throws SQLException {
+      setterCalls.add(pstmt -> pstmt.setBytes(columnIndex, data));
+      return this;
+    }
+
+    @Override
+    public SqlStatementBufferingProxy close() {
+      setterCalls.clear();
+      return this;
+    }
+  }
+
+  @FunctionalInterface
+  private interface SqlStatementSetterCall {
+    void execute(SqlStatement stmt) throws SQLException;
+  }
+
+  private static void closeUpsertImpl(UpsertImpl update) throws SQLException {
+    if (update.getBatchCount() > 0L) {
+      update.execute().commit();
+    }
+    update.close();
   }
 
 }
